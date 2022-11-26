af0413b44438a7373ecc56cea18973152d4e7726
dubic <udubic@gmail.com>
2022-11-26 12:38:55 +0100

    load env file

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Backend is ready")
+	//load env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 }
